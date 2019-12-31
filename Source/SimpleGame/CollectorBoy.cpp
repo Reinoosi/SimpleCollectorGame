2b51876463c89139b5dfd8752eddb70e950279ef
Rene Heikkilä <Brehe002@edu.xamk.fi>
2019-12-31 18:49:26 +0200

    Second game proto with UE4

---
+++ Source/SimpleGame/CollectorBoy.cpp
@@ -0,0 +1,143 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CollectorBoy.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/Actor.h"
+#include "Kismet/GameplayStatics.h"
+// Sets default values
+ACollectorBoy::ACollectorBoy()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	GetCapsuleComponent()->InitCapsuleSize(42.0f, 96.0f);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
+	GetCharacterMovement()->JumpZVelocity = 600.0f;
+	GetCharacterMovement()->AirControl = 0.2f;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+
+	CameraBoom->TargetArmLength = 300.0f;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+
+	bDead = false;
+
+	Power = 100.0f;
+
+}
+
+// Called when the game starts or when spawned
+void ACollectorBoy::BeginPlay()
+{
+
+	Super::BeginPlay();
+
+	
+	GetCapsuleComponent()->OnComponentBeginOverlap.AddDynamic(this, &ACollectorBoy::OnBeginOverlap);
+
+	if (Player_Power_Widget_Class != nullptr) {
+
+		Player_Power_Widget = CreateWidget(GetWorld(), Player_Power_Widget_Class);
+		Player_Power_Widget->AddToViewport();
+	}
+}
+
+// Called every frame
+void ACollectorBoy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	Power -= DeltaTime * Power_Treshold;
+
+	if (Power <= 0) {
+
+		if (!bDead) {
+
+			bDead = true;
+
+			GetMesh()->SetSimulatePhysics(true);
+			
+			FTimerHandle UnusedHandle;
+			GetWorldTimerManager().SetTimer(UnusedHandle, this, &ACollectorBoy::RestartGame, 3.0f, false);
+		}
+	}
+}
+
+// Called to bind functionality to input
+void ACollectorBoy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ACollectorBoy::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ACollectorBoy::MoveRight);
+
+}
+
+void ACollectorBoy::MoveForward(float Axis)
+{
+
+	if (!bDead) {
+
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Axis);
+	}
+}
+
+void ACollectorBoy::MoveRight(float Axis)
+{
+	if (!bDead) {
+
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, Axis);
+	}
+}
+
+void ACollectorBoy::RestartGame()
+{
+
+	UGameplayStatics::OpenLevel(this, FName(*GetWorld()->GetName()), false);
+
+}
+
+void ACollectorBoy::OnBeginOverlap(UPrimitiveComponent* HitComp, 
+	AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, 
+	bool bFromSweep, const FHitResult& SweepResult)
+{
+
+	if (OtherActor->ActorHasTag("Recharge")) {
+
+		Power += 10.0f;
+
+		if (Power > 100.0f)
+			Power = 100.0f;
+
+
+		OtherActor->Destroy();
+		
+	}
+}
+
---
+++ Source/SimpleGame/CollectorBoy_GameMode.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CollectorBoy_GameMode.h"
+#include "GameFramework/Actor.h"
+
+ACollectorBoy_GameMode::ACollectorBoy_GameMode() {
+
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ACollectorBoy_GameMode::BeginPlay() {
+
+	Super::BeginPlay();
+
+	FTimerHandle UnusedHandle;
+	GetWorldTimerManager().SetTimer(UnusedHandle, this, &ACollectorBoy_GameMode::SpawnPlayerRecharge, FMath::RandRange(2, 5), true);
+}
+
+void ACollectorBoy_GameMode::Tick(float DeltaTime) {
+
+	Super::Tick(DeltaTime);
+}
+
+void ACollectorBoy_GameMode::SpawnPlayerRecharge()
+{
+	float RandX = FMath::RandRange(Spawn_X_Min, Spawn_X_Max);
+	float RandY = FMath::RandRange(Spawn_Y_Min, Spawn_Y_Max);
+
+	FVector SpawnPosition = FVector(RandX, RandY, Spawn_Z);
+	FRotator SpawnRotation = FRotator(0.0f, 0.0f, 0.0f);
+
+	GetWorld()->SpawnActor(PlayerRecharge, &SpawnPosition, &SpawnRotation);
+}
+
